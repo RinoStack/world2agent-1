af564070a0bb789f41678eaa60f6a50134f3dccc
RinoStack <rino44296@gmail.com>
2026-06-27 08:09:19 +0300

    chore: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export class World2Agent {
+  constructor() {
+    this.initialize();
+  }
+
+  private initialize() {
+    // Initialize world perception
+  }
+
+  public perceiveWorld() {
+    // Perceive real world data
+  }
+}
